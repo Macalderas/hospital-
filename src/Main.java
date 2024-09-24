e44e9a00274174628f562f45542b928b893cfc01
Macalderas <159477202+Macalderas@users.noreply.github.com>
2024-09-23 22:50:26 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ import backEnde.UsuarioService;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear instancias de UsuarioService y LoginView
         UsuarioService usuarioService = new UsuarioService();
         LoginView loginView = new LoginView();
 
-        // Crear instancia de LoginController
         new LoginController(loginView, usuarioService);
     }
 }
