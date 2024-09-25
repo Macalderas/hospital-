9473ae6a3be69d38b8eed2bff4c3955c2ff48734
macalderas <macalderas@scl.edu.gt>
2024-09-25 11:45:49 -0600

    actualizacion de pacientView

--- src/view/PacienteView.java
+++ src/view/PacienteView.java
@@ -67,7 +67,7 @@ public class PacienteView {
     }
 
     public String getFechaNacimiento() {
-        return fechaNacimiento; 
+        return fechaNacimiento;
     }
 
     public String getHorarioCita() {
