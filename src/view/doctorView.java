8a60b1d22eb6a2fb96454826e4ae1e72254dc417
macalderas <macalderas@scl.edu.gt>
2024-10-06 22:47:43 -0600

    nueva rama de farmacia

--- src/view/doctorView.java
+++ src/view/doctorView.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-class LoginView extends JFrame {
+public class LoginView extends JFrame {
     private JTextField emailField;
     private JPasswordField passwordField;
     private JButton loginButton;
